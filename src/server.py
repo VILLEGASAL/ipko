34e342f6b2c96272f24e19f612199d0407260e8a
unknown <villegasalrandolph@gmail.com>
2026-03-17 23:07:10 +0800

    REAL IP ADDRESS

--- src/server.py
+++ src/server.py
@@ -7,10 +7,17 @@ template = Jinja2Templates(directory="templates")
 
 @app.get("/")
 def Home(req: Request):
+    # 1. Check if the header exists (provided by Render's proxy)
+    forwarded = req.headers.get("x-forwarded-for")
     
-    ip_address = req.client.host
+    if forwarded:
+        # The header can be a list: "Client-IP, Proxy1-IP, Proxy2-IP"
+        # The first IP is always the real user.
+        ip_address = forwarded.split(",")[0]
+    else:
+        # Fallback for local development
+        ip_address = req.client.host
     
     return template.TemplateResponse(req, "index.html", {
-
-        "ip" : ip_address
+        "ip": ip_address
     })
